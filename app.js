62055c15f8890910f57998ba1c813349d4774fcd
SrKoalo <lucasfcard98@gmail.com>
2025-03-16 20:26:08 -0300

    Funções para adicionar amigo e atualizar lista

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 // Desafio ONE Amigo Secreto
 // Desenvolvido por: Lucas "SrKoalo" de França
 
+let nomeAmigos = [];
+
+function adicionarAmigo(){
+    let nomeInserido = document.querySelector('input');
+    if (nomeInserido.value == ''){
+        alert('Preencha o nome antes de adicionar');
+    } else {
+        nomeAmigos.push(nomeInserido.value);
+        console.log(nomeInserido.value);
+        nomeInserido.value = '';
+        console.log(nomeAmigos);
+        atualizarLista();
+    }   
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+            for (i = 0 ; i < nomeAmigos.length; i++){
+                lista.innerHTML += `<li>${nomeAmigos[i]}</li>`;
+    }
+}
