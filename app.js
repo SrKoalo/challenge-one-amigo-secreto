6cc32ea3d03c0ff1a6556fd95684c9016203238d
SrKoalo <lucasfcard98@gmail.com>
2025-03-16 22:01:49 -0300

    Adicionada função de sorteio baseada no tamanho da lista

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 let nomeAmigos = [];
 
-function adicionarAmigo(){
+function adicionarAmigo() {
     let nomeInserido = document.querySelector('input');
     if (nomeInserido.value == ''){
         alert('Preencha o nome antes de adicionar');
@@ -21,5 +21,17 @@ function atualizarLista() {
     lista.innerHTML = '';
             for (i = 0 ; i < nomeAmigos.length; i++){
                 lista.innerHTML += `<li>${nomeAmigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    if (nomeAmigos.length == 0){
+        alert('Preencha os nomes antes de sortear');
+    } else if (nomeAmigos.length == 1){
+        alert('Preencha mais de um nome antes de sortear');
+    } else {
+        let nomeSorteado = parseInt(Math.random() * nomeAmigos.length)
+        console.log(nomeSorteado);
+        document.getElementById('listaAmigos').innerHTML = `<il>${nomeAmigos[nomeSorteado]}</il>`;
     }
 }
